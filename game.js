cde58d4d78eb2a10f84e2b59c1ddf698ec0cdaf7
D1N3TH <161382768+D1N3TH@users.noreply.github.com>
2024-03-20 23:19:28 +1300

    Update game.js

--- game.js
+++ game.js
@@ -9,9 +9,10 @@ const screenHeight = 300;
 
 var screenSelector = "start";
 var player;
-var score;
-var nextSpawn;
+var score = 0;
+var nextSpawn = 0;
 var obstacles;
+var spawnDist = 0 + 1;
 
 const playerWidth = 40;
 const playerHeight = 40;
@@ -19,68 +20,77 @@ const playerHeight = 40;
 const obstacleWidth = playerWidth;
 const obstacleHeight = playerHeight;
 
-function preload(){
+function preload() {
     gameBackground = loadImage('Images/GameBackground.webp');
     startBackground = loadImage('Images/startImage.jpg');
 }
 /*******************************************************/
 // setup()
 /*******************************************************/
-function setup(){
+function setup() {
     console.log("setup: ");
     cnv = new Canvas(windowWidth, windowHeight)
-     
-    floor = new Sprite(windowWidth/2, windowHeight, windowWidth, 4, 's')
+
+    obstacles = new Group();
+
+    floor = new Sprite(windowWidth / 2, windowHeight, windowWidth, 4, 's')
     floor.color = color('black')
-    world.gravity.y=10;
-    
+    world.gravity.y = 10;
+
     document.addEventListener("keydown", function(event) {
-    console.log("Key pressed! + player.y");
-    if (screenSelector == "start" || screenSelector == "end"){
-        screenSelector="game"
-        resetGame();
-    }else{
-        if(player.y > 270) {
         console.log("Key pressed!");
-        player.vel.y = -20;
-            }
+        if (screenSelector == "start" || screenSelector == "end") {
+            screenSelector = "game"
+            resetGame();
         }
-    });
-    
-    document.addEventListener("keydown", function(event) {
-        if(event.code === 'ArrowUp') {
-            player.vel.y = -5;
-        }
-        else if(event.code === 'ArrowDown') {
-            player.vel.y = 5;
-        }
-    });
-    document.addEventListener("keyup", function(event) {
-        if(event.code === 'ArrowUp') {
-            player.vel.y = 0;
-        }
-        else if(event.code === 'ArrowDown')
+    })
+};
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === 'ArrowUp') {
+        player.vel.y = -5;
+    } else if (event.code === 'ArrowDown') {
+        player.vel.y = 5;
+    }
+});
+document.addEventListener("keyup", function(event) {
+    if (event.code === 'ArrowUp') {
         player.vel.y = 0;
-    });
-}
+    } else if (event.code === 'ArrowDown')
+        player.vel.y = 0;
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === 'KeyW') {
+        player.vel.y = -5;
+    } else if (event.code === 'KeyS') {
+        player.vel.y = 5;
+    }
+});
+document.addEventListener("keyup", function(event) {
+    if (event.code === 'KeyW') {
+        player.vel.y = 0;
+    } else if (event.code === 'KeyS')
+        player.vel.y = 0;
+});
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-    if(screenSelector == "game"){
+    if (screenSelector == "game") {
         gameScreen();
-    }else if(screenSelector == "end"){
+    } else if (screenSelector == "end") {
         endScreen();
-    }else if(screenSelector == "start"){
+    } else if (screenSelector == "start") {
         startScreen();
-    }else {
+    } else if (screenSelector == "instructions") {
         text("wrong screen - you shouldnt get here", 50, 50);
         console.log("wrong screen - you shouldnt get here")
     }
 }
 
-function newObstacle(){
-    obstacle = new Sprite((screenWidth -100), screenHeight - obstacleHeight/2, obstacleWidth, obstacleHeight, 'k');
+function newObstacle() {
+    obstacle = new Sprite((screenWidth - 100), screenHeight - obstacleHeight / 2, obstacleWidth, obstacleHeight, 'k');
     obstacle.color = color("lightgreen");
     obstacle.vel.x = -10;
     obstacles.add(obstacle);
@@ -92,7 +102,7 @@ function youDead(_player, _obstacle) {
     obstacles.removeAll();
 }
 
-function startScreen(){
+function startScreen() {
     console.log("Start screen")
     background(startBackground);
 
@@ -104,17 +114,17 @@ function startScreen(){
     text("Welcome to the game", 50, 50);
     textSize(24);
     text("Press any key to start", 50, 110);
-    }
+}
 
-function gameScreen(){
-    background(gameBackground); 
+function gameScreen() {
+    background(gameBackground);
     allSprites.visible = true;
     score++;
-    if(frameCount> nextSpawn){
+    if (frameCount > nextSpawn) {
         newObstacle();
-        nextSpawn = frameCount + random(10,100);
+        nextSpawn = frameCount + random(10, 100);
     }
-    
+
     textSize(32);
     fill(255);
     stroke(0);
@@ -122,7 +132,7 @@ function gameScreen(){
     text(score, 50, 50);
 }
 
-function endScreen(){
+function endScreen() {
     console.log("YouDied")
     background("red")
     allSprites.visible = false;
@@ -132,16 +142,31 @@ function endScreen(){
     strokeWeight(4);
     text("You died! Too bad :-(", 50, 50);
     textSize(24);
-    text("your score was: "+score, 50, 110);
+    text("your score was: " + score, 50, 110);
     textSize(14);
     text("press any key to restart", 50, 150);
 }
 
-function resetGame(){
-    player = new Sprite(playerWidth*1.2, screenHeight/2, playerWidth, playerHeight, 'd');
-    player.color=color("yellow")
+function resetGame() {
+    player = new Sprite(playerWidth * 1.2, screenHeight / 2, playerWidth, playerHeight, 'd');
+    player.color = color("yellow")
     player.collides(obstacles, youDead);
-    score=0;
+    score = 0;
+}
+
+function instructions() {
+    console.log("Instructions")
+    background("red")
+    allSprites.visible = false;
+    textSize(32);
+    fill(255);
+    stroke(0);
+    strokeWeight(4);
+    text("Instructions");
+    textSize(24);
+    text("Use Up/Down Arrow Keys or W/S Keys to move player up and down");
+    textSize(14);
+    text("Avoid obstacles, collect coins, and try get a higher score");
 }
 /*******************************************************/
 //  END OF APP
